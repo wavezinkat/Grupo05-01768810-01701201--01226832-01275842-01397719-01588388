eac85aff6e52b473956f0dbf1a8272dfdfa4d2d5
Rosilene Oliveira <60991563+Rhoss88@users.noreply.github.com>
2025-04-04 09:43:24 -0300

    Update MainActivity.kt

--- Android-Studio/Contador_Numerico/app/src/main/java/com/example/contador_numerico/MainActivity.kt
+++ Android-Studio/Contador_Numerico/app/src/main/java/com/example/contador_numerico/MainActivity.kt
@@ -5,13 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton  // <--- ######## INCLUIR ESTE IMPORT
 
 class MainActivity : AppCompatActivity() {
+	 private var count = 0  // <--- ######### INCLUIR VARIAVEL COUNT
+
+    @SuppressLint("MissingInflatedId") // <--- ######### INCLUIR
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        
+
+
+	button.setOnClickListener{    	// <--- ######### INCLUIR
+            count++			// <--- ######### INCLUIR
+            countText.text = count.toString()	// <--- ######### INCLUIR
+        }				// <--- ######### INCLUIR
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
