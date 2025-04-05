30b389a903ddde2b29f24675b3d4a9a98ab1bf3b
Joaovictor21816621 <joaovictor21816621@gmail.com>
2025-04-04 21:52:47 -0300

    Update build.gradle.kts

--- Android-Studio/Contador_Numerico/app/build.gradle.kts
+++ Android-Studio/Contador_Numerico/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.contador_numerico"
+    namespace = "com.example.contador"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.contador_numerico"
+        applicationId = "com.example.contador"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -33,6 +33,61 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+
+	// ####################### CÓDIGO MODIFICADO DO build.gradle.kts (module.app)
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.contador"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.contador"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures{		// ########### INCLUIR
+        dataBinding = true	// ########### INCLUIR
+        viewBinding = true	// ########### INCLUIR
+    }				// ########### INCLUIR
 }
 
 dependencies {
