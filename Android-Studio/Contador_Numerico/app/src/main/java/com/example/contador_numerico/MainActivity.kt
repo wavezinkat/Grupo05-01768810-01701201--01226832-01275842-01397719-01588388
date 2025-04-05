81f802509fa9c84fd10dce7db0d16923a85318ee
Mateus Lira <mateus.lrg@gmail.com>
2025-04-04 21:56:30 -0300

    Update MainActivity.kt

--- Android-Studio/Contador_Numerico/app/src/main/java/com/example/contador_numerico/MainActivity.kt
+++ Android-Studio/Contador_Numerico/app/src/main/java/com/example/contador_numerico/MainActivity.kt
@@ -5,22 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompaT
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton  // <--- ######## INCLUIR ESTE IMPORT
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-	 private var count = 0  // <--- ######### INCLUIR VARIAVEL COUNT
+	 private var count = 0  
 
-    @SuppressLint("MissingInflatedId") // <--- ######### INCLUIR
+    @SuppressLint("MissingInflatedId") 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
 
-	button.setOnClickListener{    	// <--- ######### INCLUIR
-            count++			// <--- ######### INCLUIR
-            countText.text = count.toString()	// <--- ######### INCLUIR
-        }				// <--- ######### INCLUIR
+	button.setOnClickListener{ 
+            count++	
+            countText.text = count.toString()
+        }	
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
